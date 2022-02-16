edfe8d9780fa6297af66abe4c967b3524e1f1b20
Caue Venancio <caue_venancio@hotmail.com>
2022-02-15 23:19:03 -0800

    todo list - first commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,7 @@ const Task = (props) => {
     return <div key={props.id} className="task">
             <div className="task-row">
                 <h4>{props.title} {(props.subTasks.length > 0)? (<svg className="hover-hand" onClick={()=>toggleSubTasks(props.id)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 21l-12-18h24z"/></svg>):""}</h4>
-                <p>Due: {props.dueDate}<button onClick={deleteTask} className="remove-task-icon">x</button></p>         
+                <p>Due: {props.dueDate}<button onClick={deleteTask} className="remove-task-icon"> x </button></p>         
             </div>
             <div className="sub-task-row">
                 {props.subTasks.map(subtask => <p>{subtask}</p>)}
